a0eed166c253daf748261ab3b697ea3233137a88
Nora Vis <nora.viso@gmail.com>
2022-11-15 19:11:13 +0200

    routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import Back from "../src/Components/Back/Back";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Front from "./Components/Front/Front";
+import LoginPage from "./Components/LoginPage";
+import LogoutPage from "./Components/LogoutPage";
+import RequireAuth from "./Components/RequireAuth";
+import SignUp from "./Components/Front/SignUp";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes className="navbar">
+        <Route index element={<Front show="all" />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/logout" element={<LogoutPage />} />
+        <Route path="children" element={<Front show="children" />} />
+        <Route path="drama" element={<Front show="drama" />} />
+        <Route path="scifi" element={<Front show="sci-fi" />} />
+        <Route path="romance" element={<Front show="romance" />} />
+        <Route path="sign-up" element={<SignUp />} />
+        <Route
+          path="/admin"
+          element={
+            <RequireAuth>
+              <Back />
+            </RequireAuth>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
-
 export default App;
